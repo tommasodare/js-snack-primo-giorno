ea19a95cb72fdfb82995483ecf24376b165b6436
tommaso.dare <tommaso.dare96@gmail.com>
2025-02-13 12:29:26 +0100

    Done Blocco-3

--- blocco-3/main.js
+++ blocco-3/main.js
@@ -1,3 +1,13 @@
 /* Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti. */
 
+
+let sum = 0;
+
+for (let index = 0; index < 10; index++) {
+    const userNumber = Number(prompt("Type a number"));
+    console.log(userNumber);
+    sum = sum + userNumber
+}
+
+console.log(sum);
