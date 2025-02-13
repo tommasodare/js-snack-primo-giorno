f049ec5096310c098f6d9d9c9e6da38960a2c891
tommaso.dare <tommaso.dare96@gmail.com>
2025-02-13 11:19:30 +0100

    Blocco-2

--- blocco-1/main.js
+++ blocco-1/main.js
@@ -1,14 +1,15 @@
 /* L’utente inserisce due numeri in successione, con due prompt.
-
 Il software stampa il maggiore */
 
-const firstNumber = prompt("Inserisci un numero a scelta")
-const secondNumber = prompt("Inserisci un numero a scelta")
+const firstNumber = Number(prompt("Inserisci un numero a scelta"))
+const secondNumber = Number(prompt("Inserisci un numero a scelta"))
 
-if (firstNumber > secondNumber) {
-    console.log(firstNumber);
+if (isNaN(firstNumber) || isNaN(secondNumber)) {
+    alert("Invalid")
+} else if (firstNumber > secondNumber) {
+    console.log(`Il primo numero ${firstNumber} è maggiore del secondo numero ${secondNumber}`);
 } else if (firstNumber < secondNumber) {
-    console.log(secondNumber);
+    console.log(`Il secondo numero ${secondNumber} è maggiore del primo numero ${firstNumber}`);
 } else {
     console.log("I due numeri sono uguali");
 }
---
+++ blocco-2/main.js
@@ -0,0 +1,6 @@
+/* L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga. */
+
+const firstWord = prompt("Type first word")
+const secondWord = prompt("Type second word")
+
