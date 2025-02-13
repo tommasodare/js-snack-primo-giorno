8e3dd9cb309b96a7d80131388dbb38846c9ac739
tommaso.dare <tommaso.dare96@gmail.com>
2025-02-13 11:23:02 +0100

    Done Blocco-2

--- blocco-2/main.js
+++ blocco-2/main.js
@@ -4,3 +4,16 @@ Il software stampa prima la parola più corta, poi la parola più lunga. */
 const firstWord = prompt("Type first word")
 const secondWord = prompt("Type second word")
 
+const myFirstWord = firstWord.length
+const mySecondWord = secondWord.length
+
+if (myFirstWord > mySecondWord) {
+    console.log(firstWord);
+    console.log(secondWord);
+} else if (myFirstWord < mySecondWord) {
+    console.log(secondWord);
+    console.log(firstWord);
+} else {
+    console.log("The words are equal long");
+    
+}
