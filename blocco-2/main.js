8ebe6d87211f61a65ca1a1857928856cd93f14ff
tommaso.dare <tommaso.dare96@gmail.com>
2025-02-13 11:24:17 +0100

    Fix Blocco-2

--- blocco-2/main.js
+++ blocco-2/main.js
@@ -7,13 +7,12 @@ const secondWord = prompt("Type second word")
 const myFirstWord = firstWord.length
 const mySecondWord = secondWord.length
 
-if (myFirstWord > mySecondWord) {
+if (myFirstWord < mySecondWord) {
     console.log(firstWord);
     console.log(secondWord);
-} else if (myFirstWord < mySecondWord) {
+} else if (myFirstWord > mySecondWord) {
     console.log(secondWord);
     console.log(firstWord);
 } else {
     console.log("The words are equal long");
-    
 }
